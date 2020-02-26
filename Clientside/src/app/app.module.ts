75f774d4ba69ff51b0f057e8cb04e3e4790bd761
Ashish <chintakindiashish160@gmail.com>
2020-02-26 12:26:25 +0530

    2nd commit

--- Clientside/src/app/app.module.ts
+++ Clientside/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpModule } from '@angular/http';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
