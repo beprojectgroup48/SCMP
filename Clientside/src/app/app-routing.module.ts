1050ae9eaea8e8f745d30bb36734ff7a37114841
Avinash Shinde <be.projectgroup48@gmail.com>
2020-02-27 11:15:25 +0530

    second commit

--- Clientside/src/app/app-routing.module.ts
+++ Clientside/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { RegisterComponent } from './Register/Components/register.component';
 import { MainPageComponent } from './Main Page/Components/mainpage.component';
 
 const routes: Routes = [
-  {path:'login', component:MainPageComponent},
+  {path:'', component:MainPageComponent},
+  {path:'login', component:LoginComponent},
   {path:'register', component:RegisterComponent},
   {path:'profile', component:RegisterComponent}
 ]
