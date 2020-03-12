fb32569ee2f3437fbaa2267ea0548f915b0e7067
Ashish <chintakindiashish160@gmail.com>
2020-03-12 19:43:54 +0530

    third comment

--- Clientside/src/app/Distributor/Components/Dashboard/dis-dashboard.component.ts
+++ Clientside/src/app/Distributor/Components/Dashboard/dis-dashboard.component.ts
@@ -13,6 +13,7 @@ export class DistributorDashboardComponent implements OnInit {
   dataSource = ELEMENT_DATA;
   BarChart=[];
   PieChart=[];
+  PieChart2=[];
  
   constructor() { }
 
@@ -100,8 +101,49 @@ options: {
  }
 }
 });
-  }
 
+this.PieChart2 = new Chart('pieChart2', {
+    type: 'pie',
+  data: {
+   labels: ["Completed", "Pending", "Delayed", "On Time"],
+   datasets: [{
+       label: '# of Votes',
+       data: [9,7 , 3, 5],
+       backgroundColor: [
+           'rgba(255, 99, 132, 0.2)',
+           'rgba(153, 102, 255, 0.2)',         
+           'rgba(255, 206, 86, 0.2)',
+           'rgba(75, 192, 192, 0.2)'
+          //  'rgba(153, 102, 255, 0.2)',
+          //  'rgba(255, 159, 64, 0.2)'
+       ],
+       borderColor: [
+           'rgba(255,99,132,1)',
+           'rgba(54, 162, 235, 1)',
+           'rgba(255, 206, 86, 1)',
+           'rgba(75, 192, 192, 1)'
+          //  'rgba(153, 102, 255, 1)',
+          //  'rgba(255, 159, 64, 1)'
+       ],
+       borderWidth: 1
+   }]
+  }, 
+  options: {
+   title:{
+       text:"Pie Chart",
+       display:true
+   },
+   scales: {
+       yAxes: [{
+           ticks: {
+               beginAtZero:true
+           }
+       }]
+   }
+  }
+  });
+  
+}
 }
 
 //import {Component} from '@angular/core';
